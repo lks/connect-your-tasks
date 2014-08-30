fdd6fe34adc76e86e0a54c68ff215f3cb452f058
Joris Calabrese <joris.calabrese@gmail.com>
2014-08-30 23:54:48 +0200

    Write the first test with BDD usage

--- Service/UserService.php
+++ Service/UserService.php
@@ -8,11 +8,30 @@
 
 namespace Service;
 
+use GuzzleHttp\Client;
+
 
 class UserService {
 
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
     public function getKanbanizeApiKey($login, $password)
     {
-        return null;
+
+        $res = $this->client->post('http://kanbanize.com/index.php/api/kanbanize/login/email/'. urlencode($login) . '/pass/' . $password,
+            [
+                'headers' =>[
+                    'apikey' => 'saL5YcK6vQgklKJr5fBLiHH6AjgG4EbyRHly1tbM'
+                ]
+            ]);
+
+        $children = $res->xml()->children();
+        var_dump("" . $children->apikey);
+        return "" . $children->apikey;
     }
 } 
